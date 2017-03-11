1dc18b8aa6bb7a844fc888a99160644be9bbc5e0
Lylyy <jory.lydia@gmail.com>
2017-03-11 20:01:31 +0100

    Navigation - partie 1

--- js/main.js
+++ js/main.js
@@ -5,42 +5,61 @@ $(function(){
 
    var startButtonPressed = false;
    var clickOnPage = false;
+   var actualEcran; // méfie toi :/ A mon avis quand tu auras des ancres et tout ça va te porter prejudice que le actual ecran soit par défaut sur homepage...
+
+   console.log(startButtonPressed, clickOnPage);
 
 
-
+   
   // PRESS START BUTTON
-  		
-  $( "body" ).keypress(function() {
-    startButtonPressed = true;
-  });
 
- $( "body" ).click(function() {
-    clickOnPage= true;
-  });
+  function buttonPressed() {
 
-
-  if ( (startButtonPressed = true) || (clickOnPage = true) ) {
-
-        $(".pressStart").hide();
-        $( "nav" ).addClass("menu");
-        $(".title").addClass("startPress");
-        $( "#title" ).addClass( "col-lg-offset-1" );
-        console.log("coucou");
-     
+      $(".pressStart").hide();
+      $( "nav" ).addClass("menu");
+      $(".title").addClass("startPress");
+      $(".homepage").addClass("actualEcran");
+      $( "#title" ).removeClass( "col-lg-offset-3" ).addClass( "col-lg-offset-1" );
   }
 
+  $( "body" ).keypress(function() {
+    startButtonPressed = true;
+    buttonPressed();
+
+  });
+
+   $( "body" ).click(function() {
+    clickOnPage= true;
+    buttonPressed()
+  });
+
+
+          
 
  // SYSTHEME DE NAVIGATION PAR ECRAN
 
-  //$('.homepage').css({'height': hauteurFenetre});
+//$('.homepage').css({'height': hauteurFenetre});
 
   
-  $('.page').click(function() {
+  $('.lauch').click(function() {
 
-    $('.homepage').hide();
-    $(".contact").css({"display": "block"});
-    
+    $('.actualEcran').hide();
+
+    var slide = $(this).attr('data-slide');
+    $(slide).css( "display", "block" );
+    console.log ("affichez moi cette page contact u___u")
+        
   });
+
+  $('.return').click(function() {
+      var slide = $(this).attr('data-slide');
+      $(slide).css( "display", "none" );
+      $(".homepage").addClass("actualEcran");
+      $(".homepage").css( "display", "block" );
+
+  });
+
+
 
 // FIN JQUERY
 });
@@ -54,3 +73,8 @@ function lastUpdate() {
     var date = document.lastModified;
     document.getElementById("lastUpdate").innerHTML = date;
 }
+
+
+
+
+
