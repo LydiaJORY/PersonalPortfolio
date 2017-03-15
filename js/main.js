dd6a86a5846f423621c11e3827b0344105919fbd
Lylyy <jory.lydia@gmail.com>
2017-03-15 07:17:26 +0100

    Homepage menu animation

--- js/main.js
+++ js/main.js
@@ -1,40 +1,70 @@
 $(function(){
 
-  // var hauteurFenetre   = $( window ).height();
-  // var millieuFenetre = hauteurFenetre / 2;
+    // var hauteurFenetre   = $( window ).height();
+    // var millieuFenetre = hauteurFenetre / 2;
 
-   var startButtonPressed = false;
-   var clickOnPage = false;
-   var actualEcran; // méfie toi :/ A mon avis quand tu auras des ancres et tout ça va te porter prejudice que le actual ecran soit par défaut sur homepage...
-
-   console.log(startButtonPressed, clickOnPage);
-
+    var startButtonPressed = false;
+    var clickOnPage = false;
+    var actualEcran; // méfie toi :/ A mon avis quand tu auras des ancres et tout ça va te porter prejudice que le actual ecran soit par défaut sur homepage...
 
    
-  // PRESS START BUTTON
 
-  function buttonPressed() {
+
+
+
+    // PRESS START BUTTON
+
+    function preeeStartbuttonPressed() {
 
       $(".pressStart").hide();
       $( "nav" ).addClass("menu");
       $(".title").addClass("startPress");
       $(".homepage").addClass("actualEcran");
       $( "#title" ).removeClass( "col-lg-offset-3" ).addClass( "col-lg-offset-1" );
-  }
-
-  $( "body" ).keypress(function() {
-    startButtonPressed = true;
-    buttonPressed();
-
-  });
-
-   $( "body" ).click(function() {
-    clickOnPage= true;
-    buttonPressed()
-  });
 
 
-          
+    }
+
+    $( "body" ).keypress(function() {
+        startButtonPressed = true;
+        preeeStartbuttonPressed();
+    });
+
+    $( "body" ).click(function() {
+        clickOnPage= true;
+        preeeStartbuttonPressed();
+    });
+
+
+    // SELECTION MENU ANIMATION ^^
+  
+
+      $(document).on('keyup', function(e) {
+
+          if(e.which == 38){ // up arrow
+              var selected_item = $('.selected');
+
+              if(typeof selected_item.prev()[0] !== 'undefined') {
+                  selected_item.prev().addClass('selected');
+                  selected_item.removeClass('selected');
+
+              }
+          } else if (e.which == 40) { // down arrow
+              var selected_item = $('.selected');
+
+              if (typeof selected_item.next()[0] !== 'undefined') {
+                  selected_item.next().addClass('selected');
+                  selected_item.removeClass('selected');
+              }
+          }
+
+          if (e.keyCode == 13 || e.keyCode == 32) { // enter or space
+              
+  
+          }
+      });
+
+
 
  // SYSTHEME DE NAVIGATION PAR ECRAN
 
@@ -43,23 +73,29 @@ $(function(){
   
   $('.lauch').click(function() {
 
-    $('.actualEcran').hide();
+    $(".homepage").removeClass("actualEcran");
+    $(".homepage").hide();
+
 
     var slide = $(this).attr('data-slide');
     //$(slide).load("contact.html");  
     $(slide).css( "display", "block" );
-    console.log ("affichez moi cette page contact u___u")
-        
+    $(slide).addClass("actualEcran");
+    
+
   });
 
   $('.return').click(function() {
       var slide = $(this).attr('data-slide');
       $(slide).css( "display", "none" );
+       $(slide).addClass("actualEcran");
+
       $(".homepage").addClass("actualEcran");
       $(".homepage").css( "display", "block" );
-      console.log("retourner sur la homepage");
+      alert("retourner sur la homepage");
 
-  });
+    });
+
 
 
 
