3507bdbec9ed4fa36f3edf3e03e1a735ef03795d
Lylyy <jory.lydia@gmail.com>
2017-04-13 11:13:28 +0200

    Lightbox + slider fonctionel

--- js/travaux.js
+++ js/travaux.js
@@ -13,7 +13,6 @@ $(function(){
 	}); 
 
 	// lightbox
-
 	$(projetsBloc).click(function(e){ 
 
 		var $slider = $(this).find('.sliderWrap').clone(); //Clone est très important, ça sert à COPIER (CLONER) l'image mais sans modifier l'originale !
@@ -48,6 +47,49 @@ $(function(){
 	    $lightbox.fadeOut(function() {
 			$lightbox.find('.sliderWrap').remove();
 		});
+	});
+
+
+
+	// slider
+
+	var slideActive = 1;
+
+	$('.lightbox').on('click', '.slider__prev', function(e) {
+
+		var $slider_wrap 	= $('.sliderWrap'), //La virgule est fait exprès, on déclare autant de variables qu'on veut à la fois (séparés par des virgules !)
+		$slide 	 		= $('.slider'),
+		nbSlides			= $(this).parent().find('li').length; 
+
+		if(slideActive > 1) {
+			$slide.css({'marginLeft': '+=850px'});
+			
+			slideActive--; //On décrémente slideActive pour mettre à juor la position au sein du slide
+		}
+		else {
+			//Si on est déjà à la première slide et qu'on clique sur précédent, on "flashforeward" vers la dernière slide
+			$slide.css({'marginLeft' : '-' + 850 * (nbSlides-1)  + 'px'});
+			slideActive = nbSlides; //On décrémente slideActive pour mettre à jour la position au sein du slide
+		}
+	});
+
+	
+
+	$('.lightbox').on('click', '.slider__next', function() {
+
+		var $slider_wrap	= $('.sliderWrap'), //La virgule est fait exprès, on déclare autant de variables qu'on veut à la fois (séparés par des virgules !)
+		$slide 	 		= $('.slider'),
+		nbSlides			= $(this).parent().find('li').length; 
+
+		if(slideActive < nbSlides) {
+			$slide.css({'marginLeft' : '-=850px'});
+			slideActive++;
+		}
+		else {
+			//Si on est déjà à la fin, et qu'on clique à droite, on retourne en "flashback" à la première slide
+			$slide.css({'marginLeft' : '0'});
+			slideActive = 1;
+		}
 	});
 
 });
